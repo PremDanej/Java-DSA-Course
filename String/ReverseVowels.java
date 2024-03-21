19d733da32ccc9627d67cc0fe8a83e7704e91019
PremDanej <danejprem005@gmail.com>
2024-03-21 13:33:01 +0530

    Update ReverseVowels - New method added.

--- String/ReverseVowels.java
+++ String/ReverseVowels.java
@@ -6,7 +6,28 @@ package String;
 public class ReverseVowels {
     public static void main(String[] args) {
         String str = "welcome to india my friend";
-        reverseVowels(str);
+        // reverseVowels(str); // old method
+        reverseVowel(str); // new method
+    }
+
+    private static void reverseVowel(String str) {
+        char[] s = str.toCharArray();
+        int i = 0;
+        int j = str.length() - 1;
+
+        while (i < j) {
+            while (!isVowel(s[i])) i++;
+            while (!isVowel(s[j])) j--;
+            if (i >= j) break;
+
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
+            i++;
+            j--;
+        }
+        System.out.println(s);
     }
 
     private static void reverseVowels(String str) {
